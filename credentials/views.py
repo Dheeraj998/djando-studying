2a81624f57d7932e3bd7f73162f0b07373515235
Dheeraj998 <kannandheerajk@gmal.com>
2022-07-07 16:01:45 +0530

    login and logout

--- credentials/views.py
+++ credentials/views.py
@@ -1,8 +1,23 @@
-from django.contrib import messages
+from django.contrib import messages,auth
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 # Create your views here.
+
+def login(request):
+    if request.method=='POST':
+        username=request.POST['username']
+        password=request.POST['password']
+        print(password)
+        print(username)
+        user=auth.authenticate(username=username,password=password)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            messages.info(request,"inalid credentials")
+            return redirect('login')    
+    return render(request,'login.html')
 def register(request):
     if request.method=='POST':
         username=request.POST['username']
@@ -22,8 +37,15 @@ def register(request):
                 user=User.objects.create_user(username=username,password=password,first_name=firstname,last_name=lastname,email=email)
                 user.save()
                 print('user created')
-                return render(request,'/')
+                return redirect(request,'home')
         else:
             messages.info(request,'password not match')    
             return redirect('register')   
     return render(request,'register.html')
+
+def home(request):
+    return render(request,'home.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
